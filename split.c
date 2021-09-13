c9094e3d04edff0195fafed3b8d49b5122113f78
Simon Wautelet <swautele@fu-r8-p1.s19.be>
2021-09-13 19:51:42 +0200

    autosave

--- split.c
+++ split.c
@@ -6,11 +6,20 @@
 /*   By: swautele <swautele@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/10 15:41:57 by swautele          #+#    #+#             */
-/*   Updated: 2021/09/13 19:05:12 by swautele         ###   ########.fr       */
+/*   Updated: 2021/09/13 19:49:49 by swautele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include"libft.h"
+
+static void	ft_free(char **r, int l)
+{
+	while (l >= 0)
+	{
+		free(r[l]);
+		l--;
+	}
+}
 
 static size_t	ft_count(const char *str, const char c)
 {
@@ -51,16 +60,23 @@ char	**ft_split(const char *str, char c)
 	l = -1;
 	if (!str)
 		return (0);
-	r = malloc (sizeof(char *) * ft_count(str, c));
+	r = ft_calloc (sizeof(char *), ft_count(str, c) + 1);
 	if (r == NULL)
 		return (NULL);
 	while (*str)
 	{
 		while (*str == c)
 			str++;
+		if (!*str)
+			break ;
 		i = 0;
 		l++;
-		r[l] = malloc (sizeof(char) * (1 + ft_len(str, c)));
+		r[l] = ft_calloc (sizeof(char), (ft_len(str, c)));
+		if (r[l] == NULL)
+		{
+			ft_free(r, l);
+			return (NULL);
+		}
 		while (*str && *str != c)
 		{
 			r[l][i] = *str;
@@ -69,8 +85,6 @@ char	**ft_split(const char *str, char c)
 		}
 		if (*str == c || *str == '\0')
 			r[l][i] = '\0';
-		while (*str == c)
-			str++;
 	}
 	return (r);
 }
