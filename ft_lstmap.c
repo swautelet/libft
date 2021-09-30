deb2138b33e83340a1daba1783cc6e7b621aec33
Simon Wautelet <simonwautelet@MBP-de-Simon.lan>
2021-09-30 17:37:08 +0200

    autosave

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -3,47 +3,38 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstmap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: swautele <swautele@student.42.fr>          +#+  +:+       +#+        */
+/*   By: simonwautelet <simonwautelet@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/27 12:59:04 by swautele          #+#    #+#             */
-/*   Updated: 2021/09/27 15:16:11 by swautele         ###   ########.fr       */
+/*   Updated: 2021/09/30 17:32:01 by simonwautel      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include"libft.h"
 
+static void	ft_lstcopy(t_list *lst, t_list *new, void *(*f)(void *))
+{
+	if (lst)
+	{
+		new->next = ft_lstnew(f(lst->content));
+		ft_lstcopy(lst->next, new->next, f);
+	}
+	else
+		new = NULL;
+}
+
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new;
-	t_list	*in;
 
 	if (!lst || !*f)
 		return (NULL);
-	new = ft_lstnew((*f)(lst->content));
-	if (!new)
+	new = ft_lstnew(f(lst->content));
+	ft_lstcopy(lst, new, f);
+	if (ft_lstsize(new) != ft_lstsize(lst))
 	{
 		ft_lstclear(&new, del);
 		return (NULL);
 	}
-	while (lst->next)
-	{
-		lst = lst->next;
-		in = ft_lstnew(f(lst->content));
-		if (!in)
-		{
-			ft_lstclear(&new, del);
-			ft_lstclear(&in, del);
-			return (NULL);
-		}
-		ft_lstadd_back(&new, in);
-	}
-	in = ft_lstnew(f(lst->content));
-	if (!in)
-	{
-		ft_lstclear(&new, del);
-		ft_lstclear(&in, del);
-		return (NULL);
-	}
-	ft_lstadd_back(&new, in);
 	return (new);
 }
